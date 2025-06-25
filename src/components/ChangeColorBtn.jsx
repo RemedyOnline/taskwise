fb343e6686715d0edcfc9235dec50c33dee9b2b8
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2025-06-25 04:20:12 +0000

    done studying about CLSX, TWMERGE and CN

---
+++ src/components/ChangeColorBtn.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { cn } from "../lib/utils";
+
+const ChangeColorBtn = ({
+	className,
+	classNamee,
+	btnName,
+	isPrimary,
+	variant,
+}) => {
+	function getVariantStyle(variant) {
+		switch (variant) {
+			case "primary":
+				return "text-lg text-blue-500  hover:underline";
+			case "secondary":
+				return "text-lg text-red-400 font-median hover:italic";
+			case "tertiary":
+				return "text-lg text-green-600 font-mono font-black hover:uppercase";
+			default:
+				return "text-lg";
+		}
+	}
+	return (
+		<div className="flex gap-12 justify-center">
+			<button
+				className={cn(
+					"bg-amber-200 border-4 text-amber-500 border-amber-500 font-medium font-mono p-2 rounded-2xl",
+					className
+				)}
+			>
+				{btnName}
+			</button>
+			<button
+				className={cn("font-light underline italic text-gray-800", {
+					"not-italic font-bold text-red-600": isPrimary,
+				})}
+			>
+				Primary Button
+			</button>
+			<button className={cn(getVariantStyle(variant), classNamee)}>
+				Variant Button
+			</button>
+		</div>
+	);
+};
+
+export default ChangeColorBtn;
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TestBtnComponent from "../components/TestBtnComponent";
 import { cn } from "../lib/utils";
+import ChangeColorBtn from "../components/ChangeColorBtn";
 
 const TestPage = () => {
 	const color = "green";
@@ -10,6 +11,13 @@ const TestPage = () => {
 		green: "text-green-500",
 	};
 	const border = true;
+
+	const bgColor = "blue";
+	const bgColorCollection = {
+		blue: "bg-blue-200",
+		red: "bg-red-200",
+		amber: "bg-amber-200",
+	};
 
 	return (
 		<main>
@@ -33,6 +41,24 @@ const TestPage = () => {
 					styling={"bg-red-300 border-red-400"}
 				/>
 			</div>
+			<section className="bg-cyan-100 w-full h-fit p-24 text-center flex flex-col gap-6 items-center">
+				<h2 className="text-2xl font-black">
+					DAY 2 - learning how to use "clsx", "twMerge" and "cn"
+				</h2>
+				<p className={cn(bgColorCollection[bgColor], "w-96 text-center p-4")}>
+					So basically, to use <strong>clsx</strong> & <strong>twMerge</strong>{" "}
+					more efficiently, you install both packages, references them in a
+					utility file, and the us <strong>cn</strong> to call and use them at
+					once everytime...
+				</p>
+				<ChangeColorBtn
+					className="border-red-500 bg-red-100 text-red-500 font-black font-serif rounded-lg px-5"
+					btnName="Hello"
+					isPrimary={true}
+					variant={"primary"}
+					classNamee={"bg-pink-200 p-3 rounded-xl border-4 border-pink-500 bor"}
+				/>
+			</section>
 		</main>
 	);
 };
