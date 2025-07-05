4d8c41842c88dab09e3154cbea86613dc447e3dd
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2025-07-05 18:55:01 +0000

    so far so good!

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,10 +3,9 @@ import { AiOutlineDashboard } from "react-icons/ai";
 import { FaChevronLeft } from "react-icons/fa";
 import { PiChartLineUpBold, PiClockCountdownFill } from "react-icons/pi";
 import { cn } from "../lib/cnUtils";
-import { LuChartLine, LuListChecks, LuUserRound } from "react-icons/lu";
+import { LuListChecks, LuUserRound } from "react-icons/lu";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbLogout2 } from "react-icons/tb";
-import { CgProfile } from "react-icons/cg";
 import { TiChartPieOutline } from "react-icons/ti";
 
 const navItems = [
@@ -51,25 +50,38 @@ const SideBar = () => {
 		<section>
 			<aside
 				className={cn(
-					"h-screen bg-blushPink-300 shadow-lg shadow-accent flex flex-col flex-shrink-0 w-full ",
-					{ "w-18": collapsed }
+					"h-screen bg-blushPink-300 shadow-lg shadow-accent flex flex-col flex-shrink-0 w-44 md:w-48 lg:w-56 transition-all duration-500 ease-in-out ",
+					{ "w-18 md:w-18 lg:w-18": collapsed }
 				)}
 			>
 				{/* Logo and Collapse Btn... */}
 				<div
 					className={cn("flex gap-2 justify-between items-center p-4", {
-						"p-5.5 pb-4": collapsed,
+						"p-3.5 pb-4": collapsed,
 					})}
 				>
-					<div className="flex gap-1 items-center hover:cursor-pointer">
+					<div
+						className={cn("flex gap-1 items-center", {
+							"hover:bg-blushPink-400 p-2 pr-0 rounded-md group": collapsed,
+						})}
+					>
 						<PiClockCountdownFill
-							className="w-6 h-6 md:w-7 md:h-7 lg:w-8 lg:h-8 pt-0.5 lg:pt-1 flex-shrink-0"
+							className="w-6 h-6 md:w-7 md:h-7 lg:w-8 lg:h-8 pt-0.5 lg:pt-1 flex-shrink-0 hover:cursor-pointer"
 							onClick={toggleSidebar}
 						/>
+						<span>
+							{collapsed && (
+								<p className="absolute whitespace-nowrap text-xs font-medium px-2 py-1 bg-accent text-white rounded-md -mt-3 ml-2 hidden group-hover:block transition z-50">
+									Expand Sidebar
+								</p>
+							)}
+						</span>
 						<h2
 							className={cn(
-								"textbase md:text-xl lg:text-2xl font-bold text-center ",
-								{ hidden: collapsed }
+								"text-lg md:text-xl lg:text-2xl origin-left font-bold text-center opacity-100 scale-100 transition-all duration-1000",
+								{
+									"opacity-0 scale-0 ": collapsed,
+								}
 							)}
 						>
 							TaskWise
@@ -79,14 +91,14 @@ const SideBar = () => {
 						<FaChevronLeft
 							onClick={toggleSidebar}
 							className={cn(
-								"p-2 rounded-md hover:bg-accent hover:cursor-pointer",
+								"p-2 rounded-md hover:bg-blushPink-400 hover:cursor-pointer",
 								{ hidden: collapsed }
 							)}
 							size={30}
 						/>
 						<span>
 							{!collapsed && (
-								<p className="absolute whitespace-nowrap text-xs font-medium px-2 py-1 bg-accent text-white rounded-md mt-1 ml-1 hidden group-hover:block transition opacity-90">
+								<p className="absolute whitespace-nowrap text-xs font-medium px-2 py-1 bg-accent text-white rounded-md mt-0.5 ml-1 hidden group-hover:block transition">
 									Collapse Sidebar
 								</p>
 							)}
