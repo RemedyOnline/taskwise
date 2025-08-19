f9622ce30818d6282bc0bcb7d247c6e2148344ae
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2025-08-19 14:37:10 +0000

    fix signup error 2

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,24 +1,26 @@
 import apiClientMain from "./apiClientMain";
 
 // SIGN UP...
-export async function registerUser(payload) {
-	// payload = {firstName, lastName, email, password, confirmPassword}
-	const res = await apiClientMain.post("/users/register", payload);
-	console.log(payload);
-	console.log("Sign up successful..");
-
-	return res.status === 200; // per documentation...
-}
-
 // export async function registerUser(payload) {
-// 	try {
-// 		const res = await apiClientMain.post("/users/register", payload);
-// 		return res.status === 200;
-// 	} catch (err) {
-// 		console.error("Register error details:", err.response?.data || err.message);
-// 		throw err; // let the form display it
-// 	}
+// 	// payload = {firstName, lastName, email, password, confirmPassword}
+// 	const res = await apiClientMain.post("/users/register", payload);
+// 	console.log(payload);
+// 	console.log("Sign up successful..");
+
+// 	return res.status === 200; // per documentation...
 // }
+
+export async function registerUser(payload) {
+	try {
+		const res = await apiClientMain.post("/users/register", payload);
+		console.log(payload);
+		console.log("Sign up successful..");
+		return res.status === 200;
+	} catch (err) {
+		console.error("Register error details:", err.response?.data || err.message);
+		throw err; // let the form display it
+	}
+}
 
 // LOG IN...
 export async function loginUser(credentials) {
